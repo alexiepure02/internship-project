b90e34b3c2f0116f8ed76d3cec637032d43863dc
Alex Iepure <alex.iepure02@gmail.com>
2022-07-26 15:17:44 +0300

    add user and messages classes

---
+++ ConsoleChatApp/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChatApp
+{
+    public class Message
+    {
+        public int IdSender { get; set; }
+        public int IdReceiver { get; set; }
+        public string Text { get; set; }
+        public string DateTime { get; set; }
+
+    }
+}
