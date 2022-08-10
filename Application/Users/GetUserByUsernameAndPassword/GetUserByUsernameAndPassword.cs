1498624a16081437b5a3d0ba767bf022e3c6dd96
Alex Iepure <alex.iepure02@gmail.com>
2022-08-10 12:42:27 +0300

    added MediatR and created operation template

---
+++ Application/Users/GetUserByUsernameAndPassword/GetUserByUsernameAndPassword.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleChatApp.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.GetUserByUsernameAndPassword
+{
+    public class GetUserByUsernameAndPassword : IRequest<User>
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Application/Users/GetUserByUsernameAndPassword/GetUserByUsernameAndPasswordHandler.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleChatApp.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.GetUserByUsernameAndPassword
+{
+    public class GetUserByUsernameAndPasswordHandler : IRequestHandler<GetUserByUsernameAndPassword, User>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByUsernameAndPasswordHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<User> Handle(GetUserByUsernameAndPassword request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
