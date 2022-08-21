01243e39ab5e87235b755aab79409fbab146bb3e
Alex Iepure <alex.iepure02@gmail.com>
2022-08-21 19:02:30 +0300

    db context working

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -12,42 +12,66 @@ namespace Infrastructure
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Message> Messages { get; set; }
-        //public DbSet<Friends> Friends { get; set; }
-        //public DbSet<FriendRequests> FriendRequests { get; set; }
+        public DbSet<Friends> Friends { get; set; }
+        public DbSet<FriendRequests> FriendsRequests { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder builder) =>
+    protected override void OnConfiguring(DbContextOptionsBuilder builder) =>
             builder.UseSqlServer(@"Data Source=IEPURE\SQLEXPRESS;DataBase=InternshipProjectDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-            //modelBuilder.Entity<User>().HasMany(u => u.MainUserFriends).WithMany();
+            // user id 
 
-            //modelBuilder.Entity<User>().Property(u => u.Username).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.ID).ValueGeneratedNever();
 
-            modelBuilder.Entity<Friends>()
-            .HasKey(f => new { f.IDUser, f.IDFriend });
+            // many-to-many friends
 
             modelBuilder.Entity<Friends>()
                 .HasOne(f => f.User)
                 .WithMany(mu => mu.MainUserFriends)
-                .HasForeignKey(f => f.IDUser).OnDelete(DeleteBehavior.Restrict);
+                .HasForeignKey(f => f.IDFriend)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Friends>()
                 .HasOne(f => f.Friend)
                 .WithMany(mu => mu.Friends)
-                .HasForeignKey(f => f.IDFriend);
+                .HasForeignKey(f => f.IDUser);
 
-            modelBuilder.Entity<User>().HasMany(u => u.Messages).WithOne(u => u.Sender).HasForeignKey(u => u.IDSender).OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<Message>().HasOne(u => u.Receiver);
+            // one-to-many messages
 
-            // same thing for friend requests
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Messages)
+                .WithOne(u => u.Sender)
+                .HasForeignKey(u => u.IDSender)
+                .OnDelete(DeleteBehavior.Restrict);
 
-            // add validatione
+            modelBuilder.Entity<Message>()
+                .HasOne(u => u.Receiver);
 
-            // update code when using Friends - .ID not the same as .IDSender
+            // one-to-many friend requests
+            
+            modelBuilder.Entity<FriendRequests>()
+                .HasOne(u => u.User)
+                .WithMany(u => u.FriendRequests)
+                .OnDelete(DeleteBehavior.Restrict);
 
-            // modelBuilder.Entity<User>().HasMany(m => m.Followers).WithMany(p => p.Following).Map(w => w.ToTable("User_Follow").MapLeftKey("UserId").MapRightKey("FollowerID"));
+            // user validation
+
+            modelBuilder.Entity<User>().Property(x => x.ID).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.Username).HasMaxLength(50);
+            modelBuilder.Entity<User>().Property(x => x.Username).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.Password).HasMaxLength(50);
+            modelBuilder.Entity<User>().Property(x => x.Password).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.DisplayName).HasMaxLength(50);
+            modelBuilder.Entity<User>().Property(x => x.DisplayName).IsRequired();
+
+            // message validation
+
+            modelBuilder.Entity<Message>().Property(x => x.IDSender).IsRequired();
+            modelBuilder.Entity<Message>().Property(x => x.IDReceiver).IsRequired();
+            modelBuilder.Entity<Message>().Property(x => x.Text).HasMaxLength(256);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
