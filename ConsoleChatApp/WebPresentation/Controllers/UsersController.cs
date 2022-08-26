f50351b44e45354714c485242f039a1fd55326b8
Alex Iepure <alex.iepure02@gmail.com>
2022-08-26 11:35:10 +0300

    added asp.net api

---
+++ ConsoleChatApp/WebPresentation/Controllers/UsersController.cs
@@ -0,0 +1,7 @@
+﻿namespace WebPresentation.Controllers
+{
+    public class UsersController
+    {
+        //private readonly IMapper _mapper;
+    }
+}
---
+++ ConsoleChatApp/WebPresentation/Dto/UserGetDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebPresentation.Dto
+{
+    public class UserGetDto
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public virtual IList<Friends> Friends { get; set; }
+
+    }
+}
