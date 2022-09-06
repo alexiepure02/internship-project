97c0098a7ab9e18fb9f353f511205b56914e1dbe
Alex Iepure <alex.iepure02@gmail.com>
2022-09-06 17:53:06 +0300

    added annotations to message and friend request dtos

--- ConsoleChatApp/WebPresentation/Dto/FriendRequestPutPostDto.cs
+++ ConsoleChatApp/WebPresentation/Dto/FriendRequestPutPostDto.cs
@@ -1,8 +1,12 @@
-﻿namespace WebPresentation.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPresentation.Dto
 {
     public class FriendRequestPutPostDto
     {
+        [Required]
         public int IDUser { get; set; }
+        [Required]
         public int IDRequester { get; set; }
     }
 }
--- ConsoleChatApp/WebPresentation/Dto/MessagePutPostDto.cs
+++ ConsoleChatApp/WebPresentation/Dto/MessagePutPostDto.cs
@@ -1,9 +1,14 @@
-﻿namespace WebPresentation.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPresentation.Dto
 {
     public class MessagePutPostDto
     {
+        [Required]
         public int IDSender { get; set; }
+        [Required]
         public int IDReceiver { get; set; }
+        [Required]
         public string Text { get; set; }
 
     }
